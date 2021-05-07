2005d233d1f0320161b69d41884f9ec09dfb6469
Vladislav <Vladislav@DESKTOP-PEMSAR5>
2021-05-07 16:50:48 +0500

    Устранение ошибки в адресной строке /FaceCalc

--- src/main/java/laba4/Calc.java
+++ src/main/java/laba4/Calc.java
@@ -15,11 +15,16 @@ import javax.servlet.http.HttpServletResponse;
 public class Calc extends HttpServlet {
 
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		RequestCalc Calc = RequestCalc.fromRequestParameters(request);
 		Calc.setAsRequestAttributesAndCalculate(request);
 		 
-		request.getRequestDispatcher("/FaceCacl.jsp").forward(request, response);
+		request.getRequestDispatcher("/FaceCalc.jsp").forward(request, response);
 		
 	}
 	
