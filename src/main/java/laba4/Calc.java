7e329599dd49149efbc40088e392f22fc335c316
artem <artem@DESKTOP-3MR7GP8>
2021-05-07 20:55:43 +0500

    check

--- src/main/java/laba4/Calc.java
+++ src/main/java/laba4/Calc.java
@@ -46,9 +46,17 @@ public class Calc extends HttpServlet {
 		public void setAsRequestAttributesAndCalculate(HttpServletRequest request) {
 			request.setAttribute("first_result", first_calc);
 			request.setAttribute("second_result", second_calc);
-			String a,b;
-			a=first_calc.toString();
-			b=second_calc.toString();
+			int a,b;
+			try {
+				a=Integer.parseInt(first_calc);
+				b=Integer.parseInt(second_calc);
+		    } catch (NumberFormatException e) {
+		    	a=0;
+		    	b=0;
+		    }
+		        
+		    
+			
 			if (first_calc=="") {
 				
 				request.setAttribute("result", "Гость");;
@@ -58,7 +66,7 @@ public class Calc extends HttpServlet {
 			request.setAttribute("result", first_calc);;
 			request.setAttribute("result", first_calc);
 				}
-			if (a=="admin" && b=="admin") {
+			if (a==6 && b==6) {
 				
 				
 				} else {
