50cf70d0c8b496793df8c5af1803288721377664
79610 <79610@LAPTOP-LURKDULK>
2021-05-08 03:09:01 +0500

    Добавлено переопределение абстрактного класса Level

---
+++ src/main/java/laba4/Level2.java
@@ -0,0 +1,20 @@
+package laba4;
+
+public class Level2 extends Level {
+	//Создание экземпляра
+	
+	public Level2(double kf){
+		setKf(kf);
+	}
+
+	
+	
+	@Override
+	public void check() {
+	
+		
+	int [] mass = {10,20,30,40,50,60,70,800};
+		
+		
+	}
+}
