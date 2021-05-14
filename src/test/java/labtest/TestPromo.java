21f86cbd1bef380c2c13bb4307e5611bcd319e4a
Vladislav <Vladislav@DESKTOP-PEMSAR5>
2021-05-14 21:59:15 +0500

    Создание каталога для junit, и создание первого теста

---
+++ src/test/java/labtest/TestPromo.java
@@ -0,0 +1,14 @@
+package labtest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestPromo {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
