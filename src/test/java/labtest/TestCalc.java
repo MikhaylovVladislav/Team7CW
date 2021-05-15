de8b8d9991415dbe27309b08a854b41f28a05851
Vladislav <Vladislav@DESKTOP-PEMSAR5>
2021-05-15 05:37:28 +0500

    Создание теста на проверку

---
+++ src/test/java/labtest/TestCalc.java
@@ -0,0 +1,28 @@
+package labtest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import laba4.Inter2;
+
+public class TestCalc implements Inter2{
+
+
+   
+double expected = xsum1(0,0,0,0);
+
+	
+	@Test
+	  public void testToDouble() throws Exception {
+	   
+
+	          Assert.assertNotNull(expected);
+	      
+
+	}
+	}
+
+	
