0aff2da0c956ccbe6e022f3dae0dbd6ca4fe4176
Quja <Quja@DESKTOP-SES7F5N>
2021-05-15 06:04:07 +0500

    Создание теста

--- src/test/java/labtest/TakhaevTest.java
+++ src/test/java/labtest/TakhaevTest.java
@@ -1,5 +1,24 @@
 package labtest;
 
-public class TakhaevTest {
+import static org.junit.Assert.*;
 
-}
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import laba4.Inter2;
+
+public class TakhaevTest implements Inter2{
+
+
+   
+	
+
+	
+	@Test
+	  public void testTakhaev() throws Exception {
+			//boolean zxc = sum5>=0;
+			//assertTrue(zxc);
+	      
+	}
+	}
