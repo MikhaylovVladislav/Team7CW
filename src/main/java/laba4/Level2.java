b90b3134fced8fb2f19484a183eba4f8231aa1af
79610 <79610@LAPTOP-LURKDULK>
2021-05-22 20:40:13 +0500

    Работа над промокодом

--- src/main/java/laba4/Level2.java
+++ src/main/java/laba4/Level2.java
@@ -25,18 +25,16 @@ public class Level2 extends Level {
 	//	String[] mass = {"USATU", "123"};
 		int a = 123;
 		int b;
-		Float c;
+		double c;
 		
 		try {b = Integer.parseInt(promo);} catch (NumberFormatException e) {b=0;}
-		try {c = Float.parseFloat(Kfpromo);} catch (NumberFormatException e) {c = (float) 1;}
+		try {c = Double.parseDouble(Kfpromo);} catch (NumberFormatException e) {c = 1;}
 		 
 		//if ((promo == mass[0]) | (promo == mass[1])) {
 
 			if (b == a ) {
 				x=0.7;
-				if (c != 1) {
-					x=c;
-					}	
+
 				
 			}
 
