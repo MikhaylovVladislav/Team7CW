cc870372635d857357047b5fe9bc3136dcf9ecc0
79610 <79610@LAPTOP-LURKDULK>
2021-05-22 18:36:00 +0500

    Изменение коэффицента

--- src/main/java/laba4/CalcPDF.java
+++ src/main/java/laba4/CalcPDF.java
@@ -189,6 +189,7 @@ public class CalcPDF extends HttpServlet implements Inter1,Inter2 {
 	        try {intradp=Integer.parseInt(TextSrok);} catch (NumberFormatException e) {intradp=0;}
 	        try {sostg=Integer.parseInt(TextSost); } catch (NumberFormatException e) {sostg=0;}
 	        try {prcg=Integer.parseInt(TextPromo);} catch (NumberFormatException e) {prcg=0;}
+	        try {prcg=Integer.parseInt(TextKfpromo);} catch (NumberFormatException e) {prcg=0;}
 	        
 	        // состояние ремонта
 	        switch (sostg){ 
@@ -218,9 +219,10 @@ public class CalcPDF extends HttpServlet implements Inter1,Inter2 {
 			
 			Level2 kf=new Level2(1);
 			String test = TextPromo; 
+			String test1 = TextKfpromo; 
 			// промокод
 			  if (prcg==1234) {r=0.3;}	
-		    r=kf.promo(test);
+		    r=kf.promo(test, test1);
 			Inter1 i1 = new CalcPDF();
 			Inter2 i2 = new CalcPDF();
 		//	Арифметика
@@ -268,6 +270,7 @@ if (sum5==0) {z=0;}else {z=0.02;}
 			request.setAttribute("col", Textcol);
 			request.setAttribute("col1", Textcol1);
 			request.setAttribute("col2", Textcol2);
+			request.setAttribute("Kfpromo", TextKfpromo);
 		}
 
 	}
