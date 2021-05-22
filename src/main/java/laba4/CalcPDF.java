684d54adf45ca126e588e1689fb9dff19a9a5747
79610 <79610@LAPTOP-LURKDULK>
2021-05-22 19:41:00 +0500

    Работа над промокодом

--- src/main/java/laba4/CalcPDF.java
+++ src/main/java/laba4/CalcPDF.java
@@ -221,7 +221,8 @@ public class CalcPDF extends HttpServlet implements Inter1,Inter2 {
 			String test = TextPromo; 
 			String test1 = TextKfpromo; 
 			// промокод
-			  if (prcg==1234) {r=0.3;}	
+			
+			if (prcg==1234) {r=0.3;}	
 		    r=kf.promo(test, test);
 			Inter1 i1 = new CalcPDF();
 			Inter2 i2 = new CalcPDF();
